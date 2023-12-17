91b1b8017f49f3acbf044a29bb8318e9bd4d9063
OnurBahsi5566 <123368813+OnurBahsi5566@users.noreply.github.com>
2023-12-17 17:54:46 +0300

    SignalR

--- src/app/admin/layout/components/dashboard/dashboard.component.ts
+++ src/app/admin/layout/components/dashboard/dashboard.component.ts
@@ -3,6 +3,13 @@ import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 import { map } from 'rxjs/operators';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { SignalRService } from 'src/app/services/common/signal-r.service';
+import { ReceiveFunctions } from 'src/app/constants/receive-functions';
+import { HubUrls } from 'src/app/constants/hub-urls';
+import {
+  CustomToastrService,
+  ToastrMessageType,
+} from 'src/app/services/ui/custom-toastr.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,12 +17,27 @@ import { NgxSpinnerService } from 'ngx-spinner';
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent extends BaseComponent implements OnInit {
-  constructor(spinner: NgxSpinnerService) {
+  constructor(
+    spinner: NgxSpinnerService,
+    private signalRService: SignalRService,
+    private toastrService: CustomToastrService
+  ) {
     super(spinner);
+    signalRService.start(HubUrls.CargoContainerHub);
   }
 
   ngOnInit(): void {
     this.showSpinner(SpinnerType.BallAtom);
+    this.signalRService.on(
+      ReceiveFunctions.CargoContainerAddedMessageReceiveFunction,
+      (message) => {
+        console.log('neden gelmedin');
+        alert(message);
+        this.toastrService.message(message, 'Real Time!', {
+          messageType: ToastrMessageType.Info,
+        });
+      }
+    );
   }
 
   private breakpointObserver = inject(BreakpointObserver);
---
+++ src/app/constants/hub-urls.ts
@@ -0,0 +1,3 @@
+export enum HubUrls {
+  CargoContainerHub = 'https://localhost:44358/cargo-containers-hub',
+}
